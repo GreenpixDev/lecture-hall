a50e2b45d0b8a8c9f69b7d500d22bf4496855848
Anikushin Roman <anikushin.roma@gmail.com>
2023-06-22 01:20:33 +0700

    Исправление багов

--- lecture-hall/src/main/java/ru/hits/lecturehosting/hall/repository/VideoRepository.java
+++ lecture-hall/src/main/java/ru/hits/lecturehosting/hall/repository/VideoRepository.java
@@ -15,12 +15,12 @@ import java.util.UUID;
 
 public interface VideoRepository extends JpaRepository<Video, UUID> {
 
-    @Query("select v from Video v where v.group.id = :groupId and " +
+    @Query("select v from Video v where v.group.id = :groupId and v.playerUrl is not null and " +
             "(v.title like :textExpression or v.description like :textExpression) and " +
             "((select count(l) from v.labels l where l in :labels) >= :labelCount)")
     Page<Video> searchAll(UUID groupId, String textExpression, List<Label> labels, int labelCount, Pageable pageable);
 
-    @Query("select v from Video v where v.group.id = :groupId and " +
+    @Query("select v from Video v where v.group.id = :groupId and v.playerUrl is not null and " +
             "(v.title like :textExpression or v.description like :textExpression) and " +
             "((select count(l) from v.labels l where l in :labels) >= :labelCount) and " +
             "v.subject.id in :subjectIds")
